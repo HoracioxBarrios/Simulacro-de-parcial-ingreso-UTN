f3dce3801d2b6596a93ff1113d8364861cea2b1b
horacio Barrios <horacioxbarrios@gmail.com>
2023-03-01 23:45:00 -0300

    mejor

--- simulacro-parcial.js
+++ simulacro-parcial.js
@@ -28,7 +28,7 @@ NOTA: Deben poder ingresarse todos números positivos, no se deben pueden ingres
 
 *******************************************************************************/
 
-function mostrar(){
+function mostrar() {
     let nombre;
     let nacionalidad;
     let posicion;
@@ -39,15 +39,18 @@ function mostrar(){
     let flag;
     let minRojasRecibidas;
     let JugadorMenosRojasRecibidas;
-    let nacionalidadJugadorMenosRojasRecibidas;
-    let porcentajeJugadoresIngleses;
     let respuesta;
-    
+    let contadorGeneral;
+    let contadorIngleses;
+    let procentajeDeIngleses;
 
-    flag= true;
 
-   
-    do {
+    flag = true;
+    contadorGeneral = 0;
+    contadorIngleses = 0;
+
+
+    do{
 
         nombre = prompt("Ingrese nombre del jugador");
         //asi esria bien la validacion, aunque no es algo que se nos va a evaluar si ingresa un numero en este caso.
@@ -57,56 +60,71 @@ function mostrar(){
         }
         */
         nacionalidad = prompt("Ingrese nacionalidad");
-        
-    
+
+
         posicion = prompt("Posición (Arquero, Defensor, Mediocampista o Delantero)");
         posicion = posicion.toLowerCase();
         // valido la posicion
-        while(posicion != "arquero" && posicion != "defensor" 
-        && posicion != "mediocampista" && posicion != "delantero"){
+        while (posicion != "arquero" && posicion != "defensor"
+            && posicion != "mediocampista" && posicion != "delantero"){
             posicion = prompt("Incorrecto! - Posición (Arquero, Defensor, Mediocampista o Delantero)");
-            posicion = posicion.toLowerCase(); 
+            posicion = posicion.toLowerCase();
         }
-    
+
         tarjetasRojas = prompt("Cabtidad de tarjetas rojas recibidas");
-        tarjetasRojas = parseInt(tarjetasRojas);
+        tarjetasRojas = parseInt(tarjetasRojas);//parseo
         while(tarjetasRojas < 0){// valido que no entren negativos
             tarjetasRojas = prompt("Incorrecto! - ingrese Cabtidad de tarjetas rojas recibidas");
-            tarjetasRojas = parseInt(tarjetasRojas);  
+            tarjetasRojas = parseInt(tarjetasRojas);
         }
-    
+
         golMarcado = prompt("ingrese Goles marcados");
-        golMarcado = parseInt(golMarcado);
+        golMarcado = parseInt(golMarcado);//parseo
         while(golMarcado < 0){// valido que no entren negativos
             golMarcado = prompt("Numero Invalido ingrese Goles marcados");
             golMarcado = parseInt(golMarcado);
         }
-    
-        if(flag){
+
+        if (flag){// max y min 
             maxGol = golMarcado;
             nombreMaxGoleador = nombre;
+
             minRojasRecibidas = tarjetasRojas;
             flag = false;
-        }else {
+        } else{
             if(golMarcado > maxGol){
                 maxGol = golMarcado;
-                nombreMaxGoleador = nombre;//a)El goleador del torneo (nombre y max gol)
+                nombreMaxGoleador = nombre;//a)El maximo goleador del torneo (nombre y goles)
             }
             if(nacionalidad == "argentino"){
-                if(tarjetasRojas < minRojasRecibidas){
+                if (tarjetasRojas < minRojasRecibidas){
                     minRojasRecibidas = tarjetasRojas;
                     JugadorMenosRojasRecibidas = nombre;//b)El jugador Argentino con menos rojas recibidas.
                 }
-                
-
             }
         }
+
+        if (nacionalidad == "inglaterra"){
+            contadorIngleses++;
+        }
+
         respuesta = confirm("Quiere continuar ingresando?")
+        contadorGeneral++;
     }while(respuesta);
-   
-   
+
+    //c)El porcentaje de jugadores Ingleses de la liga
+    procentajeDeIngleses = (contadorIngleses * 100) / contadorGeneral;
+
+    alert("El goleador del torneo: " + nombreMaxGoleador + " cantidad: " + maxGol
+        + "El jugador argentino con menos rojas recibidas: " + JugadorMenosRojasRecibidas + "Cant. minimas rojas" + minRojasRecibidas
+        + "Porcentaje de ingleses: " + procentajeDeIngleses);
 
 }
+/*regla de 3 simples para % jug ingleses
+10  general  100
+4 ingleses
 
 
- 
+resultado = (4 * 100) /10
+ */
+
